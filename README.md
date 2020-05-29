4d558543f98cdcf02d650ab85806c100e1262051
kingspp <kingspprathyush@gmail.com>
2020-05-29 13:11:18 -0400

    Updated installer

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # turing_ide
 Integrated Development Environment for WPI's turing clusters
+
+## Installation
+Takes 10 mins so get a cup of coffee!
+
+```bash
+# Clone the repository
+git clone https://github.com/kingspp/turing_ide --recursive
+
+```
--- install.sh
+++ install.sh
@@ -6,7 +6,23 @@
 #
 '
 
-# Extract rpm files
-# Setup paths
-# Compile theia
-# run browser
+$HOMEDIR=`pwd`
+$TMPDIR=${HOMEDIR}/tmp
+mkdir -p ${TMPDIR}
+
+# Install nodejs v11.50 and Yarn
+cd ${TMPDIR}
+wget https://nodejs.org/download/release/v11.15.0/node-v11.15.0-linux-x64.tar.gz
+wget https://github.com/yarnpkg/yarn/releases/download/v1.22.4/yarn-v1.22.4.tar.gz
+tar -xvf node-v11.15.0-linux-x64.tar.gz ../node
+tar -xvf yarn-v1.22.4.tar.gz ../yarn
+PATH=${PATH}:${HOMEDIR}/node/bin:${HOMEDIR}/yarn/bin
+cd ${HOMEDIR}
+
+# Export paths. Dependencies are pre-configured in the repo. 
+export PKG_CONFIG_PATH=$HOMEDIR
+export CPATH=$HOMEDIR
+
+# Compile Theia and built extensions and examples
+yarn
+
